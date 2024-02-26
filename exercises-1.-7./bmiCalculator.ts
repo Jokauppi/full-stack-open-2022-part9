a7c0898d4ce2d3193d36c612c168ad1356a76a9d
Joona Kauppila <56773501+Jokauppi@users.noreply.github.com>
2024-02-26 14:29:21 +0200

    9.1

---
+++ exercises-1.-7./bmiCalculator.ts
@@ -0,0 +1,20 @@
+const messages: [number, string][] = [
+  [16.0, "Underweight (Severe thinness)"],
+  [17.0, "Underweight (Moderate thinness)"],
+  [18.5, "Underweight (Mild thinness)"],
+  [25.0, "Normal (Healthy Weight)"],
+  [30.0, "Overweight (Pre-obese)"],
+  [35.0, "Obese (Class I)"],
+  [40.0, "Obese (Class II)"],
+  [Infinity, "Obese (Class III)"],
+];
+
+const calculateBmi = (height: number, mass: number) => {
+  const bmi = mass / (height / 100) ** 2;
+
+  for (const category of messages) {
+    if (bmi < category[0]) return category[1];
+  }
+};
+
+console.log(calculateBmi(180, 74));
